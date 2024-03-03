b3cb787d1640f89f157baafd220fa7dbc9accc19
jefry5 <69019659+jefry5@users.noreply.github.com>
2024-03-03 00:01:51 -0500

    Add files via upload

--- JS/scriptNosotros.js
+++ JS/scriptNosotros.js
@@ -1,8 +1,7 @@
 var header = document.getElementById('Header');
-var timeout;
+
 
 window.addEventListener('scroll',()=>{
-
     var scroll = window.scrollY
 
     if (scroll>10){
@@ -11,5 +10,19 @@ window.addEventListener('scroll',()=>{
     }else{
         header.classList.remove('fade-in-out');
         setTimeout(header.style.backgroundColor = 'transparent',300);
+    }
+})
+
+//Valores animación FALTA ARREGLAR
+
+
+window.addEventListener('scroll',()=>{
+    var valoresAnimation = document.querySelector('nosotros-valores-container');
+    var scrollValor = window.scrollY
+
+    if (scrollValor > 30 ) {
+        valoresContainer.classList.add('activar-animacion');
+    } else {
+        valoresContainer.classList.remove('activar-animacion');
     }
 })
