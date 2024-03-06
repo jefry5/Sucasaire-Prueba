a9e7fb71ca4e6bcbf08adf78ad73c7717762c476
jefry5 <69019659+jefry5@users.noreply.github.com>
2024-03-06 18:14:43 -0500

    Add files via upload

--- JS/scriptServicioDiseño.js
+++ JS/scriptServicioDiseño.js
@@ -27,6 +27,49 @@ function colorNegroHeader(){
     }
 }
 
+//Animación de Expansión y Contracción de los productos
+
+const producto_1 = document.getElementById('producto-texto-1');
+const producto_2 = document.getElementById('producto-texto-2');
+const producto_3 = document.getElementById('producto-texto-3');
+
+function expandirAnimacion(elemento){
+    elemento.style.animation = 'crecimientoBox 0.5s ease forwards';
+    desparecerOtrosProductos(elemento.id);
+}
+function contraerAnimacion(elemento){
+    elemento.style.animation = 'contraerBox 0.5s ease forwards';
+    aparecerOtrosProducto();
+}
+function expandirConClick(elemento) {
+    if(elemento.id == producto_1.id){
+        producto_1.style.backgroundColor = '#cecece';
+    }else if(elemento.id == producto_2.id){
+        producto_2.style.backgroundColor = '#cecece';
+    }else{
+        producto_3.style.backgroundColor = '#cecece';
+    }
+}
+function desparecerOtrosProductos(producto){
+    if(producto == producto_1.id){
+        producto_2.style.opacity = '0.3';
+        producto_3.style.opacity = '0.3';
+    }else if(producto == producto_2.id){
+        producto_1.style.opacity = '0.3';
+        producto_3.style.opacity = '0.3';
+    }else{
+        producto_1.style.opacity = '0.3';
+        producto_2.style.opacity = '0.3';
+    }
+}
+function aparecerOtrosProducto(){
+    producto_1.style.opacity = '1';
+    producto_2.style.opacity = '1';
+    producto_3.style.opacity = '1';
+}
+
+
+
 //Actualización del año en el PIE DE PÁGINA
 var yearElement = document.getElementById('year');
 var currentYear = new Date().getFullYear();
